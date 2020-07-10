32555e72629e7e8e2ba321abf04b1930bebef009
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-07-10 21:21:57 +0100

    Added wheels to car, Added basic models

---
+++ Assets/WheelGraphics.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelGraphics : MonoBehaviour
+{
+    WheelCollider collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponentInParent<WheelCollider>();
+    }
+
+    // Update is called once per frame
+    void Fixed()
+    {
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+        this.transform.position = position;
+    }
+}
